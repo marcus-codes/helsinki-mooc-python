1bf085b5fa5f256694328c085fd0a78be6ef9438
marcus-codes <m.izaquias@gmail.com>
2025-04-09 14:00:00 +0100

    Solve: Fix the code - Seven Brothers and emoticon exercises

--- part_1/getting_started/emoticon.py
+++ part_1/getting_started/emoticon.py
@@ -1,1 +1,9 @@
-git
+"""
+Helsinki MOOC - Part 1: Getting Started
+Exercise: Emoticon
+
+Task: Create a program that prints an emoticon using Unicode characters.
+Example output: 😀
+"""
+
+print("\U0001F600")  # Grinning face
--- part_1/getting_started/fix_the_code_seven_brothers.py
+++ part_1/getting_started/fix_the_code_seven_brothers.py
@@ -1,6 +1,31 @@
 """
 Helsinki MOOC - Part 1: Getting Started
-Exercise: Fix the code: Seven Brothers
+Exercise: Fix the code - Seven Brothers
+
+Problem Statement:
+The program should print the names of the brothers from 'Seitsemän veljestä'
+in alphabetical order, but the original code has them in incorrect order.
+
+Original Code:
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")
 """
 
-# Your solution here
+# Fixed Solution
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
+
+# Alternative Solution (More Scalable)
+# brothers = ["Simeoni", "Juhani", "Eero", "Lauri", "Aapo", "Tuomas", "Timo"]
+# for name in sorted(brothers):
+#     print(name)
