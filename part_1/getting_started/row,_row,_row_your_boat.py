f3c4362b92585fd75a2cacf9abc07ef7ae84d388
marcus-codes <m.izaquias@gmail.com>
2025-04-17 14:00:00 +0100

    Solve: Row your boat exercises

--- part_1/getting_started/row,_row,_row_your_boat.py
+++ part_1/getting_started/row,_row,_row_your_boat.py
@@ -1,6 +1,19 @@
 """
 Helsinki MOOC - Part 1: Getting Started
 Exercise: Row, Row, Row Your Boat
+
+Problem Statement:
+Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+
+Sample output:
+Row, row, row your boat,
+Gently down the stream.
+Merrily, merrily, merrily, merrily,
+Life is but a dream.
 """
 
 # Your solution here
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
