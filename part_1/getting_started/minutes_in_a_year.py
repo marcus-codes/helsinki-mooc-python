8ec0d161bd0aba2caad843f027c1730b3ec921f0
marcus-codes <m.izaquias@gmail.com>
2025-04-21 14:00:00 +0100

    Solve: Minutes in a year exercise

--- part_1/getting_started/minutes_in_a_year.py
+++ part_1/getting_started/minutes_in_a_year.py
@@ -1,6 +1,11 @@
 """
 Helsinki MOOC - Part 1: Getting Started
 Exercise: Minutes in a year
+
+Problem Statement:
+Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
 """
 
 # Your solution here
+print(365*1440)
+
