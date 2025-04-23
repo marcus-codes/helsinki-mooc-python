8ce14516b56635c9464fa562ca3879f803c31c33
marcus-codes <m.izaquias@gmail.com>
2025-04-23 15:53:11 +0100

    Solved exercise 'print some code'

--- part_1/getting_started/print_some_code.py
+++ part_1/getting_started/print_some_code.py
@@ -1,6 +1,22 @@
 """
 Helsinki MOOC - Part 1: Getting Started
 Exercise: Print some code
+
+Problem Statement:
+Thus far, you have probably used double quotation marks " to print out strings. In addition to the double quotation marks, Python also accepts single quotation marks '.
+
+This comes in handy if you ever want to print out the actual quotation marks themselves:
+
+
+print('"Come right back!", shouted the police officer.')
+Sample output
+"Come right back!", shouted the police officer.
+
+Please write a program which prints out the following:
+
+Sample output
+print("Hello there!")
 """
 
 # Your solution here
+print('print("Hello there!")')
