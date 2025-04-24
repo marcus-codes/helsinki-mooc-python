20274cab9acd38311f650c0485571ebbf76d3943
marcus-codes <m.izaquias@gmail.com>
2025-04-24 16:44:51 +0100

    Solved exercise 'Name twice'

--- part_1/information_from_the_user/name_twice.py
+++ part_1/information_from_the_user/name_twice.py
@@ -1,6 +1,19 @@
 """
 Helsinki MOOC - Part 1: Information from the user
 Exercise: Name twice
+
+Problem Statement:
+Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+
+An example of the how the program should function:
+
+Sample output
+What is your name? Paul
+Paul
+Paul
 """
 
 # Your solution here
+name = input("Please enter your name")
+print(name)
+print(name)
