6ebe8e3944ef1532b1b3822465b5b4e41adc3271
marcus-codes <m.izaquias@gmail.com>
2025-04-25 15:07:25 +0100

    Solved exercise 'Name and exclamation marks'

--- part_1/information_from_the_user/name_and_exclamation_marks.py
+++ part_1/information_from_the_user/name_and_exclamation_marks.py
@@ -1,6 +1,18 @@
 """
 Helsinki MOOC - Part 1: Information from the user
 Exercise: Name and exclamation marks
+
+Problem Statement:
+Please write a program which asks for the user's name and then prints it out twice on a single line so that there is an exclamation mark at the
+beginning of the line, another between the two names and a third one at the end of the line.
+
+The program should function as follows:
+
+Sample output
+What is your name? Paul
+!Paul!Paul!
 """
 
 # Your solution here
+name = input("Enter your name")
+print("!" + name + "!" + name + "!")
