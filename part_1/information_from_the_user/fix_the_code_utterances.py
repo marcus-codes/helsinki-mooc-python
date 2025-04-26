fc96f6ee9c1f58b609d911298765d627b0b41986
marcus-codes <m.izaquias@gmail.com>
2025-04-26 17:03:06 +0100

    Solved exercise 'Fix the code: Utterances'

--- part_1/information_from_the_user/fix_the_code_utterances.py
+++ part_1/information_from_the_user/fix_the_code_utterances.py
@@ -1,6 +1,21 @@
 """
 Helsinki MOOC - Part 1: Information from the user
 Exercise: Fix the code: Utterances
+
+Problem Statement:
+Here is a program which should ask for three utterances and print them out, like so:
+
+Sample output
+The 1st part: hickory
+The 2nd part: dickory
+The 3rd part: dock
+hickory-dickory-dock!
+
+However, there is something wrong with the code below. Please fix it.
 """
 
-# Your solution here
+# Fix the code
+part_one = input("The 1st part: ")
+part_two = input("The 2ndt part: ")
+part_three = input("The 3rd part: ")
+print(part_one + "-" + part_two + "-" + part_three + "!")
