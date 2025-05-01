d4a5b962aeab967e1bed0236a5c78fb04f0060fc
marcus-codes <m.izaquias@gmail.com>
2025-05-01 23:25:09 +0100

    Exercise submitted: 'Times Five'

--- part_1/arithmetic_operations/times_five.py
+++ part_1/arithmetic_operations/times_five.py
@@ -1,6 +1,18 @@
 """
 Helsinki MOOC - Part 1: Arithmetic operations
 Exercise: Times five
+
+Problem Statement:
+
+Please write a program which asks the user for a number. The program then prints out the number multiplied by five.
+
+The program should function as follows:
+
+Sample output
+Please type in a number: 3
+3 times 5 is 15
 """
 
 # Your solution here
+number = int(input("Please type in a number:"))
+print(f"{number} times 5 is {number * 5}")
