28ad3c242da246b5c2f60f4ed7ee5d81ac57faca
marcus-codes <m.izaquias@gmail.com>
2025-05-11 17:37:32 +0100

    Submitted programming exercise: Typecasting

--- part_2/programming_terminology/typecasting.py
+++ part_2/programming_terminology/typecasting.py
@@ -3,11 +3,43 @@ Helsinki MOOC - Part 2: Programming Terminology
 Exercise: Typecasting
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+When programming in Python, often we need to change the data type of a value. For example, a floating point number can be converted into an integer with the function int:
 
-Example:
-[Include example inputs/outputs if provided]
+
+temperature = float(input("Please type in a temperature: "))
+
+print("The temperature is", temperature)
+
+print("...and rounded down it is", int(temperature))
+Sample output
+Please type in a temperature: 5.15
+The temperature is 5.15
+...and rounded down it is 5
+
+Notice the function always rounds down, and not according to the rounding rules in mathematics. This is an example of a floor function.
+
+Sample output
+Please type in a temperature: 8.99
+The temperature is 8.99
+...and rounded down it is 8
+
+Please write a program which asks the user for a floating point number and then prints out the integer part and the decimal part separately. Use the Python int function.
+
+You can assume the number given by the user is always greater than zero.
+
+An example of expected behaviour:
+
+Sample output
+Please type in a number: 1.34
+Integer part: 1
+Decimal part: 0.34
 """
 
-# Your solution here
+# Write your solution here
+number = float(input("Please type in a number: "))
+integer = int(number)
+decimal = float(number - integer)
+
+print("Integer part: ",integer)
+print("Decimal part: ",decimal)
 
