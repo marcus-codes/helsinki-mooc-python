e038d1c131f243262d4cb9a8e3d6191d96799852
marcus-codes <m.izaquias@gmail.com>
2025-05-12 16:01:03 +0100

    Submitted programming exercise: Age of maturity

--- part_2/more_conditionals/age_of_maturity.py
+++ part_2/more_conditionals/age_of_maturity.py
@@ -3,11 +3,25 @@ Helsinki MOOC - Part 2: More Conditionals
 Exercise: Age of maturity
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which asks the user for their age. The program should then print out a message based on whether the user is of age or not,
+using 18 as the age of maturity.
 
-Example:
-[Include example inputs/outputs if provided]
+Some examples of expected behaviour:
+
+Sample output
+How old are you? 12
+You are not of age!
+
+Sample output
+How old are you? 32
+You are of age!
 """
 
-# Your solution here
+# Write your solution here
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are of age!")
+else:
+    print("You are not of age!")
 
