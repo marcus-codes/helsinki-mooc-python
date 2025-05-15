4fa5d8505e1051ad7dc6ff338937aaade78a3d11
marcus-codes <m.izaquias@gmail.com>
2025-05-15 16:49:19 +0100

    Submitted programming exercise: Alphabetically last

--- part_2/more_conditionals/alphabetically_last.py
+++ part_2/more_conditionals/alphabetically_last.py
@@ -13,17 +13,17 @@ You can assume all words will be typed in lowercase entirely.
 
 Some examples of expected behaviour:
 
-Sample output
+Sample output:
 Please type in the 1st word: car
 Please type in the 2nd word: scooter
 scooter comes alphabetically last.
 
-Sample output
+Sample output:
 Please type in the 1st word: zorro
 Please type in the 2nd word: batman
 zorro comes alphabetically last.
 
-Sample output
+Sample output:
 Please type in the 1st word: python
 Please type in the 2nd word: python
 You gave the same word twice.
