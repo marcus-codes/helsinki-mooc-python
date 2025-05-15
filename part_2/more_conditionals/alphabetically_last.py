322bc061b9807f989235dc3573bdeb32cbabdbf4
marcus-codes <m.izaquias@gmail.com>
2025-05-15 16:47:10 +0100

    Submitted programming exercise: Alphabetically last

--- part_2/more_conditionals/alphabetically_last.py
+++ part_2/more_conditionals/alphabetically_last.py
@@ -3,11 +3,42 @@ Helsinki MOOC - Part 2: More Conditionals
 Exercise: Alphabetically last
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Python comparison operators can also be used on strings. String a is smaller than string b if it comes alphabetically before b. Notice however that the comparison is only reliable if
 
-Example:
-[Include example inputs/outputs if provided]
+the characters compared are of the same case, i.e. both UPPERCASE or both lowercase
+only the standard English alphabet of a to z, or A to Z, is used.
+Please write a program which asks the user for two words. The program should then print out whichever of the two comes alphabetically last.
+
+You can assume all words will be typed in lowercase entirely.
+
+Some examples of expected behaviour:
+
+Sample output
+Please type in the 1st word: car
+Please type in the 2nd word: scooter
+scooter comes alphabetically last.
+
+Sample output
+Please type in the 1st word: zorro
+Please type in the 2nd word: batman
+zorro comes alphabetically last.
+
+Sample output
+Please type in the 1st word: python
+Please type in the 2nd word: python
+You gave the same word twice.
 """
 
-# Your solution here
+# Write your solution here
+word_one = input("Please type in the 1st word: ")
+word_two = input("Please type in the 2nd word: ")
+
+if word_one < word_two:
+    print(f"{word_two} comes alphabetically last.")
+
+elif word_two < word_one:
+    print(f"{word_one} comes alphabetically last.")
+
+else:
+    print("You gave the same word twice.")
 
