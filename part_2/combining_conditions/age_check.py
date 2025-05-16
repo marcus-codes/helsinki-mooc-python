5d260a371df827412d76a8a751f91d8416877eff
marcus-codes <m.izaquias@gmail.com>
2025-05-16 16:34:30 +0100

    Submitted programming exercise: Age check

--- part_2/combining_conditions/age_check.py
+++ part_2/combining_conditions/age_check.py
@@ -3,11 +3,30 @@ Helsinki MOOC - Part 2: Combining Conditions
 Exercise: Age check
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which asks for the user's age. If the age is not plausible, that is, it is under 5 or something that can't be an actual human age, the program should print out a comment.
 
-Example:
-[Include example inputs/outputs if provided]
+Have a look at the examples of expected behaviour below to figure out which comment is applicable in each case.
+
+Sample output
+What is your age? 13
+Ok, you're 13 years old
+
+Sample output
+What is your age? 2
+I suspect you can't write quite yet...
+
+Sample output
+What is your age? -4
+That must be a mistake
 """
 
-# Your solution here
+# Write your solution here
+age = int(input("What is your age? "))
+
+if age < 5 and age >= 0:
+    print("I suspect you can't write quite yet...")
+elif age < 0:
+    print("That must be a mistake")
+else:
+    print(f"Ok, you're {age} years old")
 
