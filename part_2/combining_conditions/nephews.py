2913c03b1fea00cfb6259ade1d1ed807ce7de9c4
marcus-codes <m.izaquias@gmail.com>
2025-05-17 16:17:25 +0100

    Submitted programming exercise: Nephews

--- part_2/combining_conditions/nephews.py
+++ part_2/combining_conditions/nephews.py
@@ -3,11 +3,34 @@ Helsinki MOOC - Part 2: Combining Conditions
 Exercise: Nephews
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which asks for the user's name. If the name is Huey, Dewey or Louie, the program should recognise the user as one of Donald Duck's nephews.
 
-Example:
-[Include example inputs/outputs if provided]
+In a similar fashion, if the name is Morty or Ferdie, the program should recognise the user as one of Mickey Mouse's nephews.
+
+Some examples:
+
+Sample output
+Please type in your name: Morty
+I think you might be one of Mickey Mouse's nephews.
+
+Sample output
+Please type in your name: Huey
+I think you might be one of Donald Duck's nephews.
+
+Sample output
+Please type in your name: Ken
+You're not a nephew of any character I know of.
 """
 
-# Your solution here
+# Write your solution here
+user_name = input("Please type in your name: ")
+
+if user_name == 'Huey' or user_name == 'Dewey' or user_name == 'Louie':
+    print("I think you might be one of Donald Duck's nephews.")
+
+elif user_name == 'Morty' or user_name == 'Ferdie':
+    print("I think you might be one of Mickey Mouse's nephews.")
+
+else:
+    print("You're not a nephew of any character I know of.")
 
