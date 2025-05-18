95012a74d821f8d3c8659c329466848fc4b4eb9b
marcus-codes <m.izaquias@gmail.com>
2025-05-18 09:00:00 +0100

    Submitted programming exercise: Grades and points

--- part_2/combining_conditions/grades_and_points.py
+++ part_2/combining_conditions/grades_and_points.py
@@ -3,11 +3,48 @@ Helsinki MOOC - Part 2: Combining Conditions
 Exercise: Grades and points
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+The table below outlines the grade boundaries on a certain university course. Please write a program which asks for the amount of points received and then prints out the grade attained according to the table.
 
-Example:
-[Include example inputs/outputs if provided]
+points	grade
+< 0	impossible!
+0-49	fail
+50-59	1
+60-69	2
+70-79	3
+80-89	4
+90-100	5
+> 100	impossible!
+Some examples:
+
+Sample output
+How many points [0-100]: 37
+Grade: fail
+
+Sample output
+How many points [0-100]: 76
+Grade: 3
+
+Sample output
+How many points [0-100]: -3
+Grade: impossible!
 """
 
-# Your solution here
+# Write your solution here
+user_grade = int(input("How many points [0-100]: "))
+
+if user_grade <= 0 or user_grade > 100:
+    print("Grade: impossible!")
+elif user_grade < 50:
+    print("Grade: fail")
+elif user_grade < 60:
+    print("Grade: 1")
+elif user_grade < 70:
+    print("Grade: 2")
+elif user_grade < 80:
+    print("Grade: 3")
+elif user_grade < 90:
+    print("Grade: 4")
+else:
+    print("Grade: 5")
+
 
