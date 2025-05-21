339b31ffb992532c5d98382d368f8c3b25c4b6cb
marcus-codes <m.izaquias@gmail.com>
2025-05-21 09:00:00 +0100

    Submitted programming exercise: Alphabetically in the middle

--- part_2/combining_conditions/alphabetically_in_the_middle.py
+++ part_2/combining_conditions/alphabetically_in_the_middle.py
@@ -3,11 +3,50 @@ Helsinki MOOC - Part 2: Combining Conditions
 Exercise: Alphabetically in the middle
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which asks the user for three letters.
+The program should then print out whichever of the three letters would be in the middle if the letters were in alphabetical order.
 
-Example:
-[Include example inputs/outputs if provided]
+You may assume the letters will be either all uppercase, or all lowercase.
+
+Some examples of expected behaviour:
+
+Sample output
+1st letter: x
+2nd letter: c
+3rd letter: p
+The letter in the middle is p
+
+Sample output
+1st letter: C
+2nd letter: B
+3rd letter: A
+The letter in the middle is B
 """
 
-# Your solution here
+# Write your solution here
+letter_one = input("1st letter: ")
+letter_two = input("2nd letter: ")
+letter_three = input("3rd letter: ")
+
+middle_letter =  ""
+
+if letter_one < letter_two and letter_one > letter_three:
+    middle_letter = letter_one
+elif letter_one > letter_two and letter_one < letter_three:
+    middle_letter = letter_one
+
+if letter_two < letter_one and letter_two > letter_three:
+    middle_letter = letter_two
+elif letter_two > letter_one and letter_two < letter_three:
+    middle_letter = letter_two
+
+if letter_three < letter_one and letter_three > letter_two:
+    middle_letter = letter_three
+elif letter_three > letter_one and letter_three < letter_two:
+    middle_letter = letter_three
+
+print(f"The letter in the middle is {middle_letter}")
+
+
+
 
