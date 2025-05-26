1e51079757111232123030804a26e9e176be0fb7
marcus-codes <m.izaquias@gmail.com>
2025-05-26 09:00:00 +0100

    Submitted programming exercise: Repeat Password

--- part_2/simple_loops/repeat_password.py
+++ part_2/simple_loops/repeat_password.py
@@ -3,11 +3,30 @@ Helsinki MOOC - Part 2: Simple Loops
 Exercise: Repeat password
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which asks the user for a password. The program should then ask the user to type in the password again.
+If the user types in something else than the first password, the program should keep on asking until the user types the first password again correctly.
 
-Example:
-[Include example inputs/outputs if provided]
+Have a look at the expected behaviour below:
+
+Sample output
+Password: sekred
+Repeat password: secret
+They do not match!
+Repeat password: cantremember
+They do not match!
+Repeat password: sekred
+User account created!
 """
 
-# Your solution here
+# Write your solution here
+p1 = input("Password: ")
+p2 = input("Repeat password: ")
+
+while True:
+    if p1 == p2:
+        print("User account created!")
+        break
+    else:
+        print("They do not match!")
+        p2 = input("Repeat password: ")
 
