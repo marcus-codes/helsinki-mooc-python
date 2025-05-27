640fa6a7ac3f0ef90b95c996afa1ce1fdab7d9e4
marcus-codes <m.izaquias@gmail.com>
2025-05-27 14:05:16 +0100

    Submitted programming exercise: PIN and number of attempts

--- part_2/simple_loops/pin_and_number_of_attempts.py
+++ part_2/simple_loops/pin_and_number_of_attempts.py
@@ -3,11 +3,42 @@ Helsinki MOOC - Part 2: Simple Loops
 Exercise: PIN and number of attempts
 
 Task:
-[Copy-paste the exact task description from the MOOC here]
+Please write a program which keeps asking the user for a PIN code until they type in the correct one, which is 4321.
+The program should then print out the number of times the user tried different codes.
 
-Example:
-[Include example inputs/outputs if provided]
+Sample output
+PIN: 3245
+Wrong
+PIN: 1234
+Wrong
+PIN: 0000
+Wrong
+PIN: 4321
+Correct! It took you 4 attempts
+
+If the user gets it right on the first try, the program should print out something a bit different:
+
+Sample output
+PIN: 4321
+Correct! It only took you one single attempt!
 """
 
-# Your solution here
+# Write your solution here
+attempts = 1
+
+while True:
+    pin = input("PIN: ")
+
+    if pin == "4321":
+        break
+    else:
+        print("Wrong")
+        attempts += 1
+
+if attempts == 1:
+    print(f"Correct! It only took you one single attempt!")
+else:
+    print(f"Correct! It took you {attempts} attempts")
+
+
 
