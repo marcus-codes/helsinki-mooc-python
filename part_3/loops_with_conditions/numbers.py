f3ff821a7d4a020f49dcef52285ff0168e8f7aec
marcus-codes <m.izaquias@gmail.com>
2025-06-03 21:41:48 +0100

    Submitted programming exercise: Numbers

--- part_3/loops_with_conditions/numbers.py
+++ part_3/loops_with_conditions/numbers.py
@@ -3,10 +3,21 @@ Helsinki MOOC - Part 3: Loops With Conditions
 Exercise: Numbers
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Upper limit: 5
+1
+2
+3
+4
 """
 
 # Write your solution here
+num = int(input("Upper limit: "))
+start = 1
+
+while start < num:
+    print(start)
+    start += 1
+
