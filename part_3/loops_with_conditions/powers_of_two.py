baa3e8d75f4d13193dd57f5163a912a63c0a4f5f
marcus-codes <m.izaquias@gmail.com>
2025-06-04 12:09:01 +0100

    Submitted programming exercise: Powers of two

--- part_3/loops_with_conditions/powers_of_two.py
+++ part_3/loops_with_conditions/powers_of_two.py
@@ -3,10 +3,54 @@ Helsinki MOOC - Part 3: Loops With Conditions
 Exercise: Powers of two
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in an upper limit.
+The program then prints out numbers so that each subsequent number is the previous one doubled, starting from the number 1.
+That is, the program prints out powers of two in order.
 
-Example:
-[Add example inputs/outputs if available]
+The execution of the program finishes when the next number to be printed would be greater than the limit set by the user.
+No numbers greater than the limit should be printed.
+
+Upper limit: 8
+1
+2
+4
+8
+
+Sample output
+Upper limit: 20
+1
+2
+4
+8
+16
+
+Sample output
+Upper limit: 100
+1
+2
+4
+8
+16
+32
+64
+
+Please don't use the value True as the condition of your while loop in this exercise!
+
+What are powers of two? The first power of two is the number 1. The next one is 1 times 2, which is 2. The next is 2 times 2, which is 4.
+The next is 4 times 2, which is 8, and so forth. Each power in the sequence is multiplied by two to produce the next one.
 """
 
 # Write your solution here
+num = int(input("Upper limit: "))
+start = 1
+
+while start <= num:
+    print(start)
+    start *= 2
+
+
+
+
+
+
+
