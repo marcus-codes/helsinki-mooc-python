68f971d7454162e50cd22455b7c898c349b2aa7f
marcus-codes <m.izaquias@gmail.com>
2025-06-05 12:21:43 +0100

    Programming exercise submitted: Powers of base n

--- part_3/loops_with_conditions/powers_of_base_n.py
+++ part_3/loops_with_conditions/powers_of_base_n.py
@@ -3,10 +3,36 @@ Helsinki MOOC - Part 3: Loops With Conditions
 Exercise: Powers of base n
 
 Task:
-[Copy exercise description from MOOC here]
+Please change the program from the previous exercise so that the user gets to input also the base which is multiplied
+(in the previous program the base was always 2).
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Upper limit: 27
+Base: 3
+1
+3
+9
+27
+
+Sample output
+Upper limit: 1234567
+Base: 10
+1
+10
+100
+1000
+10000
+100000
+1000000
+
+Please don't use the value True as the condition of your while loop in this exercise!
 """
 
 # Write your solution here
+num = int(input("Upper limit: "))
+base = int(input("Base: "))
+start = 1
+
+while start <= num:
+    print(start)
+    start *= base
