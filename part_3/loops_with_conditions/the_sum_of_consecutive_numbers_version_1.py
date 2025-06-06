c95c20f2af7989880b2f7747aa31742827d37066
marcus-codes <m.izaquias@gmail.com>
2025-06-06 15:19:19 +0100

    Submitted programming exercise: The sum of consecutive numbers, version 1

--- part_3/loops_with_conditions/the_sum_of_consecutive_numbers_version_1.py
+++ part_3/loops_with_conditions/the_sum_of_consecutive_numbers_version_1.py
@@ -3,10 +3,36 @@ Helsinki MOOC - Part 3: Loops With Conditions
 Exercise: The sum of consecutive numbers, version 1
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in a limit. The program then calculates the sum of consecutive numbers
+(1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user. The program should function as follows:
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Limit: 2
+3
+
+Sample output
+Limit: 10
+10
+
+Sample output
+Limit: 18
+21
+
+If you have trouble understanding how the desired output is calculated, the sample outputs in the next exercise may help.
+You may assume the number typed in by the user is always equal to 2 or higher.
 """
 
 # Write your solution here
+limit = int(input("Limit: "))
+current = 1
+calculation = 0
+
+while calculation < limit:
+    calculation += current
+    current += 1
+
+print(calculation)
+
+
+
+
