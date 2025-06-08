89f8cad4dba8eca325308e0ba97bac709540d5f4
marcus-codes <m.izaquias@gmail.com>
2025-06-08 23:43:59 +0100

    Submitted programming exercise: String multiplied

--- part_3/working_with_strings/string_multiplied.py
+++ part_3/working_with_strings/string_multiplied.py
@@ -3,10 +3,23 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: String multiplied
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user for a string and an amount.
+The program then prints out the string as many times as specified by the amount. The printout should all be on one line,
+with no extra spaces or symbols added.
 
-Example:
-[Add example inputs/outputs if available]
+An example of expected behaviour:
+
+Sample output
+Please type in a string: hiya
+Please type in an amount: 4
+hiyahiyahiyahiya
 """
 
 # Write your solution here
+string = input("Please type in a string: ")
+amount = int(input("Please type in an amount: "))
+str_multiplied = string*amount
+
+print(str_multiplied)
+
+
