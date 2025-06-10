735a81b62df3378fdc7e8d67df659be8bd45f12c
marcus-codes <m.izaquias@gmail.com>
2025-06-10 13:20:58 +0100

    Submitted programming exercise: End to beginning

--- part_3/working_with_strings/end_to_beginning.py
+++ part_3/working_with_strings/end_to_beginning.py
@@ -3,10 +3,22 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: End to beginning
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user for a string. The program then prints out the input string in reversed order,
+from end to beginning. Each character should be on a separate line.
 
-Example:
-[Add example inputs/outputs if available]
+An example of expected behaviour:
+
+Sample output
+Please type in a string: hiya
+a
+y
+i
+h
 """
 
 # Write your solution here
+input_string = input("Please type in a string: ")
+index = -1
+while index >= -len(input_string):
+    print(input_string[index])
+    index -= 1
