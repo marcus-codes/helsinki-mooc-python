4c0454852e27667864706dd27cb125869ee2caf9
marcus-codes <m.izaquias@gmail.com>
2025-06-11 16:22:21 +0100

    Submitted programming exercise: Second and second to last characters

--- part_3/working_with_strings/second_and_second_to_last_characters.py
+++ part_3/working_with_strings/second_and_second_to_last_characters.py
@@ -3,10 +3,30 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: Second and second to last characters
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user for a string. The program then prints out a message based on whether the second character and the
+second to last character are the same or not. See the examples below.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Please type in a string: python
+The second and the second to last characters are different
+
+Sample output
+Please type in a string: pascal
+The second and the second to last characters are a
 """
 
 # Write your solution here
+
+# ask the user to input a string
+user_string = input("Please type in a string: ")
+# store the last char in the str in a variable
+second_last_char = user_string[-2]
+# store the 2nd last char in the str as a variable
+second_char = user_string[1]
+# check if both variables are equal, use an if statement
+if second_last_char == second_char:
+    print(f"The second and the second to last characters are {second_last_char}")
+else:
+    print("The second and the second to last characters are different")
+# print 1 of 2 messages, either they're different, or print the letter that they are
+
