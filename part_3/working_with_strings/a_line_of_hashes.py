7a4ad3464d0cae403ba601a490066e43e80a4735
marcus-codes <m.izaquias@gmail.com>
2025-06-12 11:35:21 +0100

    Submitted programming exercise: A line of hashes

--- part_3/working_with_strings/a_line_of_hashes.py
+++ part_3/working_with_strings/a_line_of_hashes.py
@@ -3,10 +3,25 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: A line of hashes
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Width: 3
+
+###
+Sample output
+Width: 8
+
+########
 """
 
 # Write your solution here
+
+# store the hash symbol as a variable
+hash_var = '#'
+# get the user to input how many hashes they want
+user_num_input = int(input("Width: "))
+# update the hash variable with the user input and store in a variable
+hash_var = hash_var * user_num_input
+# concatenate the hash variable by the number that the user input
+print(hash_var)
