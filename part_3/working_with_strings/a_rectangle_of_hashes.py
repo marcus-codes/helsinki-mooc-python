0434930d853b85586f73a2f9d708e10d874218d0
marcus-codes <m.izaquias@gmail.com>
2025-06-13 17:56:16 +0100

    Submitted programming exercise: A rectangle of hashes

--- part_3/working_with_strings/a_rectangle_of_hashes.py
+++ part_3/working_with_strings/a_rectangle_of_hashes.py
@@ -3,10 +3,24 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: A rectangle of hashes
 
 Task:
-[Copy exercise description from MOOC here]
+Please modify the previous program so that it also asks for the height, and prints out a rectangle of hash characters accordingly.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Width: 10
+Height: 3
+##########
+##########
+##########
 """
 
 # Write your solution here
+
+# get the width from the user
+width = int(input("Width: "))
+# get the height from the user
+height = int(input("Height: "))
+# create a for loop that creates rows based on the height input
+for _ in range(0, height, 1):
+    print('#'*width)
+
+
