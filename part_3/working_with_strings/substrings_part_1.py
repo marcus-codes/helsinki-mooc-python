0d8747cf32d2817fe44b372718cf177d34347513
marcus-codes <m.izaquias@gmail.com>
2025-06-17 14:05:17 +0100

    Submitted programming exercise: Substrings, part 1

--- part_3/working_with_strings/substrings_part_1.py
+++ part_3/working_with_strings/substrings_part_1.py
@@ -3,10 +3,22 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: Substrings, part 1
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in a string. The program then prints out all the substrings which begin with the first character,
+from the shortest to the longest. Have a look at the example below.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Please type in a string: test
+t
+te
+tes
+test
 """
 
 # Write your solution here
+
+word = input("Please type in a string: ")
+
+for i in range(1, len(word) + 1, 1):
+    print(word[0:i])
+
+
