2540db04cc07edfe3c1f0ad4fccc2e154047e56b
marcus-codes <m.izaquias@gmail.com>
2025-06-18 10:40:00 +0100

    Submitted programming exercise: Substrings part 2

--- part_3/working_with_strings/substrings_part_2.py
+++ part_3/working_with_strings/substrings_part_2.py
@@ -3,10 +3,25 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: Substrings, part 2
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in a string. The program then prints out all the substrings which end with the last character,
+from the shortest to the longest. Have a look at the example below.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Please type in a string: test
+t
+st
+est
+test
 """
 
 # Write your solution here
+
+# variable as user input
+# start a for loop that starts at -1 and moves down to the final index of the str
+# print the word with the index starting at the i var and moving to the first index
+s = input("Please type in a string: ")
+for i in range(1, len(s), +1):
+    print(s[-i:])
+
+
+
