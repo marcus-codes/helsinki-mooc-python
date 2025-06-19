bf8c43c85361f1cf6302dcba65d74636968112a0
marcus-codes <m.izaquias@gmail.com>
2025-06-19 14:41:41 +0100

    Submitted programming exercise: Does it contain vowels

--- part_3/working_with_strings/does_it_contain_vowels.py
+++ part_3/working_with_strings/does_it_contain_vowels.py
@@ -3,10 +3,31 @@ Helsinki MOOC - Part 3: Working With Strings
 Exercise: Does it contain vowels
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to input a string.
+The program then prints out different messages if the string contains any of the vowels a, e or o.
 
-Example:
-[Add example inputs/outputs if available]
+You may assume the input will be in lowercase entirely. Have a look at the examples below.
+
+Sample output
+Please type in a string: hello there
+a not found
+e found
+o found
+
+Sample output
+Please type in a string: hiya
+a found
+e not found
+o not found
 """
 
 # Write your solution here
+
+user_input = input("Please type in a string: ")
+vowels = 'aeo'
+
+for vowel in "aeo":
+    if vowel in user_input:
+        print(vowel, "found")
+    else:
+        print(vowel, "not found")
