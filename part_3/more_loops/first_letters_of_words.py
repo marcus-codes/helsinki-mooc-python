df4e83421ac3ef3a86d2652538d8963f2b41b0ae
marcus-codes <m.izaquias@gmail.com>
2025-06-24 15:36:56 +0100

    Submitted programming exercise: First letters of words

--- part_3/more_loops/first_letters_of_words.py
+++ part_3/more_loops/first_letters_of_words.py
@@ -3,10 +3,25 @@ Helsinki MOOC - Part 3: More Loops
 Exercise: First letters of words
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in a sentence.
+The program then prints out the first letter of each word in the sentence, each letter on a separate line.
 
-Example:
-[Add example inputs/outputs if available]
+An example of expected behaviour:
+
+Sample output
+Please type in a sentence: Humpty Dumpty sat on a wall
+H
+D
+s
+o
+a
+w
 """
 
 # Write your solution here
+
+sentence = input("Please type in a sentence: ")
+
+for i in range(len(sentence)):
+    if i == 0 or sentence[i - 1] == " ":
+        print(sentence[i])
