8ecaa15c3c97addafbe0fae2119786fbbd3b050d
marcus-codes <m.izaquias@gmail.com>
2025-06-26 15:06:48 +0100

    Submitted programming exercise: Flip the pairs

--- part_3/more_loops/flip_the_pairs.py
+++ part_3/more_loops/flip_the_pairs.py
@@ -3,10 +3,35 @@ Helsinki MOOC - Part 3: More Loops
 Exercise: Flip the pairs
 
 Task:
-[Copy exercise description from MOOC here]
+Please write a program which asks the user to type in a number. The program then prints out all the positive integer values from 1 up to the number.
+However, the order of the numbers is changed so that each pair or numbers is flipped. That is, 2 comes before 1, 4 before 3 and so forth.
+See the examples below for details.
 
-Example:
-[Add example inputs/outputs if available]
+Sample output
+Please type in a number: 5
+2
+1
+4
+3
+5
+
+Sample output
+Please type in a number: 6
+2
+1
+4
+3
+6
+5
 """
 
 # Write your solution here
+
+number = int(input("Please type in a number: "))
+
+for _ in range(1, number + 1, 2):
+    if _ + 1 <= number:
+        print(_ + 1)
+        print(_)
+    else:
+        print(_)
